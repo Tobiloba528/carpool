c694ea321556a87ca07928863cdbe163fbc9cfb0
Tobiloba <omitomotobiloba@gmail.com>
2023-03-05 15:28:44 -0500

    empty state of requested trip

--- screens/App/RequestedTripsScreen.js
+++ screens/App/RequestedTripsScreen.js
@@ -7,6 +7,7 @@ import {
   FlatList,
   RefreshControl,
   ScrollView,
+  Image,
 } from "react-native";
 import { FontAwesome, Ionicons } from "@expo/vector-icons";
 import React, { useState, useCallback } from "react";
@@ -144,6 +145,7 @@ const forFlat = [
 ];
 
 const RequestedTripsScreen = ({ navigation }) => {
+  const [trips, setTrips] = useState([]);
   const [items, setItems] = useState([
     { id: 1, active: true, title: "All" },
     { id: 2, active: false, title: "Trips" },
@@ -155,6 +157,7 @@ const RequestedTripsScreen = ({ navigation }) => {
     setRefreshing(true);
     setTimeout(() => {
       setRefreshing(false);
+      setTrips(dummyTrips);
     }, 2000);
   }, []);
 
@@ -164,40 +167,73 @@ const RequestedTripsScreen = ({ navigation }) => {
       <View>
         <CustomTab items={items} setItems={setItems} />
       </View>
-
-      <View style={styles.contentContainer}>
-        <FlatList
-          data={forFlat}
-          key={(item) => item.id}
-          renderItem={({}) => (
-            <View>
-              <Text style={styles.date}>Today</Text>
-              <FlatList
-                data={dummyTrips}
-                key={(item) => item.id}
-                renderItem={({ item }) => (
-                  <TripItem
-                    item={item}
-                    onPress={() => navigation.navigate("TripDetailScreen")}
-                  />
-                )}
-              />
-            </View>
-          )}
+      {trips.length == 0 ? (
+        <ScrollView
+          contentContainerStyle={styles.emptyContainer}
           refreshControl={
             <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
           }
-        />
-        <View style={styles.btnContainer}>
-          <Pressable
-            onPress={() => navigation.navigate("HeadingScreen")}
-            style={({ pressed }) => [styles.btn, pressed && styles.btnPressed]}
-          >
-            <Text style={styles.btnText}>Post</Text>
-            <Ionicons name="add" size={24} color="white" />
-          </Pressable>
+        >
+          <View style={styles.thinkContainer}>
+            <Image
+              style={styles.thinkImg}
+              source={require("../../assets/think.png")}
+            />
+          </View>
+          <Text style={styles.emptyText}>
+            Looks like there are no results matching your search
+          </Text>
+          <View style={styles.btnContainer2}>
+            <Pressable
+              onPress={() => navigation.navigate("HeadingScreen")}
+              style={({ pressed }) => [
+                styles.btn,
+                pressed && styles.btnPressed,
+              ]}
+            >
+              <Text style={styles.btnText}>Post</Text>
+              <Ionicons name="add" size={24} color="white" />
+            </Pressable>
+          </View>
+        </ScrollView>
+      ) : (
+        <View style={styles.contentContainer}>
+          <FlatList
+            data={forFlat}
+            key={(item) => item.id}
+            renderItem={({}) => (
+              <View>
+                <Text style={styles.date}>Today</Text>
+                <FlatList
+                  data={trips}
+                  key={(item) => item.id}
+                  renderItem={({ item }) => (
+                    <TripItem
+                      item={item}
+                      onPress={() => navigation.navigate("TripDetailScreen")}
+                    />
+                  )}
+                />
+              </View>
+            )}
+            refreshControl={
+              <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
+            }
+          />
+          <View style={styles.btnContainer}>
+            <Pressable
+              onPress={() => navigation.navigate("HeadingScreen")}
+              style={({ pressed }) => [
+                styles.btn,
+                pressed && styles.btnPressed,
+              ]}
+            >
+              <Text style={styles.btnText}>Post</Text>
+              <Ionicons name="add" size={24} color="white" />
+            </Pressable>
+          </View>
         </View>
-      </View>
+      )}
     </SafeAreaView>
   );
 };
@@ -210,6 +246,20 @@ const styles = StyleSheet.create({
   contentContainer: {
     flex: 1,
   },
+  emptyContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  emptyText: {
+    fontWeight: "500",
+    fontSize: 16,
+    lineHeight: 18,
+    width: "65%",
+    textAlign: "center",
+    marginVertical: 20,
+    color: "#555555",
+  },
   date: {
     fontWeight: "600",
     fontSize: 16,
@@ -218,16 +268,22 @@ const styles = StyleSheet.create({
   },
   btnContainer: {
     backgroundColor: "black",
-    borderRadius: 10,
+    borderRadius: 15,
     position: "absolute",
     bottom: 10,
     right: 20,
   },
+  btnContainer2: {
+    backgroundColor: "black",
+    borderRadius: 15,
+    overflow: "hidden",
+    width: 140,
+  },
+
   btn: {
     flexDirection: "row",
     alignItems: "center",
     paddingHorizontal: 20,
-    width: 140,
     paddingVertical: 15,
     display: "flex",
     justifyContent: "center",
@@ -235,11 +291,20 @@ const styles = StyleSheet.create({
   btnText: {
     fontWeight: "bold",
     color: "white",
+    fontSize: 17,
     marginRight: 15,
   },
   btnPressed: {
-    opacity: 0.7
-  }
+    opacity: 0.7,
+  },
+  thinkContainer: {
+    width: 50,
+    height: 50,
+  },
+  thinkImg: {
+    width: "100%",
+    height: "100%",
+  },
 });
 
 export default RequestedTripsScreen;
